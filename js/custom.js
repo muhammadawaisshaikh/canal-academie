6cb596a8795fa465ed309a803fdc48f0fed1f376
muhammadawais <awaisshaikh94@gmail.com>
2020-03-17 23:28:33 +0500

    sidenav from left

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("mySidenav").style.right = "0%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.right = "100%";
 }
 
 // slider-carousel
